213c5d31b3aa9ad2c8c7081a8ecc4da48b7d2406
manikantareddy268 <manikantareddy130263@gmail.com>
2026-01-23 00:34:19 +0530

    feat: add test file for bot review

---
+++ test_review.js
@@ -0,0 +1,10 @@
+// This is a test file to verify the AI Code Review Bot.
+// It contains intentional bad practices to see if the bot catches them.
+
+function calculateTotal(price) {
+    var tax = 0.10;
+    // Todo: fix this hardcoded value
+    return price * 1.10;
+}
+
+console.log("Total is " + calculateTotal(100));
